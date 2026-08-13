e29aee43217a3a0085aaea0f1a78c6b411bf3741
somifott <michael.fot@stud.uni-due.de>
2026-08-13 17:48:55 +0200

    Add password length tests

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -1,12 +1,29 @@
 import string
 from password.new_password import generate_password
 
+
 def test_password_characters():
     """Тест, что при генерации используются только допустимые символы"""
     valid_characters = string.ascii_letters + string.digits + string.punctuation
     password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
     for char in password:
         assert char in valid_characters
+
+
+def test_password_length():
+    """Тест, что длина пароля соответствует заданной."""
+    assert len(generate_password(20)) == 20
+
+
+def test_default_password_length():
+    """Тест, что длина пароля по умолчанию равна 12."""
+    assert len(generate_password()) == 12
+
+
+def test_zero_length_password():
+    """Тест, что при нулевой длине возвращается пустая строка."""
+    assert generate_password(0) == ''
+
 
 """
 Допиши еще один тест из предложенных. Или придумай свой.
