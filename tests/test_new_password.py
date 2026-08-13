8403984d11ea5799c60216f91243e7e8e83bcc3c
kubzero10-cpu <kubzero.1.0@gmail.com>
2026-08-13 19:25:20 +0300

    Добавленно 2 теста

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -1,6 +1,5 @@
 import string
 from password.new_password import generate_password
-
 def test_password_characters():
     """Тест, что при генерации используются только допустимые символы"""
     valid_characters = string.ascii_letters + string.digits + string.punctuation
@@ -8,10 +7,16 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
-"""
-Допиши еще один тест из предложенных. Или придумай свой.
-Если сможешь написать больше, то будет круто!
 
-Тест, что длина пароля соответствует заданной
-Тест, что два сгенерированных подряд пароля различаются
-"""
+def test_possword_lenght():
+    """Тест длины"""
+    test_lengths = [8, 12, 16, 20, 32, 50]
+    for length in test_lengths:
+        password = generate_password(length)
+        assert len(password) == length
+
+def test_passwords_different():
+    """Тест, что два сгенерированных подряд пароля различаются"""
+    p1 = generate_password(10)
+    p2 = generate_password(10)
+    assert p1 != p2
