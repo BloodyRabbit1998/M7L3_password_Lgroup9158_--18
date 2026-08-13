b82143deda9858ec16b0e3301d1cacfda20e2e46
unknown <Islam05.06.13@yandex.ru>
2026-08-13 21:23:55 +0500

    Выполненые коментария в ./tests/test_new_password.py

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,10 +8,23 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
-"""
-Допиши еще один тест из предложенных. Или придумай свой.
-Если сможешь написать больше, то будет круто!
 
-Тест, что длина пароля соответствует заданной
-Тест, что два сгенерированных подряд пароля различаются
-"""
+###Допиши еще тесты из предложенных.
+def test_lenght_generate_password():
+    ###Тест, что длина пароля соответствует заданной
+    password = generate_password(101)
+    count_for_password = 0
+    for char in password:
+        count_for_password += 1
+    assert count_for_password == 101
+
+def test_same_generate_password():
+    ###Тест, что два сгенерированных подряд пароля различаются
+    password = generate_password(777)
+    password7 = generate_password(777)
+    if password != password7:
+        stroki = 'raznie'
+    else:
+        stroki = 'odinakovie'
+    assert stroki == 'raznie'
+
